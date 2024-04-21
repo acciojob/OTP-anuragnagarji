92252eda015681e83aa70d2d2c233a5bfb7906ce
anuragnagarji <142933378+anuragnagarji@users.noreply.github.com>
2024-04-21 12:04:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let inputs=document.getElementsByClassName("code");
+/*let inputs=document.getElementsByClassName("code");
 window. onload = function() { 
     for(let inEle of inputs){
         inEle.value="";
@@ -32,7 +32,7 @@ function lastInput(e){
         t.collapse(true);
         t.moveEnd('character', len);
         t.moveStart('character', len);
-        t.select();*/
+        t.select();*//*
     }
        // console.log("hello"+(index-1));
 
@@ -52,6 +52,32 @@ if(isNaN(e.key)){ //if not a number enter user
        
        index= Number(ind.slice(-1));
        inputs[index].focus();
-    }
+    }*/
 	
-}
+//}
+const codes = document.querySelectorAll(".code")
+ 
+codes[0].focus()
+ 
+codes.forEach((code,idx)=>{
+    code.addEventListener("keydown",(e)=>{
+        console.log(e.key)
+        const keyPressed = e.key
+ 
+        if(keyPressed>=0 && keyPressed<=9){
+            codes[idx].value=''
+            if(idx<codes.length-1){
+                setTimeout(()=>codes[idx+1].focus(),10)
+                 setTimeout(function(){
+                    codes[index+1].focus()
+                 },10)
+            }
+        }
+        else if(keyPressed==="Backspace"){
+            if(idx>0){
+                setTimeout(()=>codes[idx-1].focus(),10)
+            }
+
+        }
+    })
+})
